573c0f2a1c2c7fa5a8f7a049cd7d68da6d9cb21a
khalilsediq <khalilullahsadique1334@gmail.com>
2025-09-23 14:25:50 +0500

    added

---
+++ app.js
@@ -0,0 +1,15 @@
+// Q1    
+// Answer
+// function strdate(){
+// let currentdate = new Date()
+// return currentdate.toDateString().toString()
+// }
+// console.log( strdate());
+
+// Q2    
+// Answer
+
+let timeone = new Date ("2025-09-23T08:15:00");
+let timetwo = new Date ("2025-09-23T10:45:00");
+const time
+
