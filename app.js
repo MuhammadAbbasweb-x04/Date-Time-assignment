4249562eb84ec0f7382f8ef1549041d10ee405a8
khalilsediq <khalilullahsadique1334@gmail.com>
2025-09-23 15:17:33 +0500

    added

--- app.js
+++ app.js
@@ -8,8 +8,24 @@
 
 // Q2    
 // Answer
+// let timeone = new Date ()
+// let timetwo = new Date ("2025-09-23T10:45:00")
+// const timeculculate = timeone > timetwo;
+// const timedef = Math.floor(timeculculate  * 1000 * 60 );
+// console.log(timedef);
 
-let timeone = new Date ("2025-09-23T08:15:00");
-let timetwo = new Date ("2025-09-23T10:45:00");
-const time
 
+
+// Q3    
+// Answer
+// let brithdate = new Date ("March 6, 2003")
+// let currentdate = new Date()
+// let remningseconds = brithdate -currentdate ;
+// const culculateage = Math.abs(Math.floor(remningseconds / 1000 / 60 /60 / 24/ 365))
+// console.log(culculateage);
+
+// Q4    
+// Answer
+// const currentdate = new Date();
+// const firstday = new Date(currentdate.getFullYear(), currentdate.getMonth(), 1 )
+// console.log(firstday);
